6fa92b8a27cbdba94722f3d9b01d35a486fc8af7
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-02-06 21:25:22 -0800

    Added gsap and animated category cards

--- src/categories/CategoriesList.js
+++ src/categories/CategoriesList.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { listCategories } from "../utils/api";
 import { useNavigate } from "react-router-dom";
 import CategoryCard from "./CategoryCard";
+import gsap from "gsap";
 
 const CategoriesList = () => {
   const navigate = useNavigate();
+
+  const cardRef = useRef();
+  const singleCard = gsap.utils.selector(cardRef);
 
   const [categories, setCategories] = useState(null);
   const [errors, setErrors] = useState(null);
@@ -16,8 +20,22 @@ const CategoriesList = () => {
     return () => abortController.abort();
   }, []);
 
+  useEffect(() => {
+    gsap.fromTo(
+      singleCard(".card"),
+      { opacity: 0, x: 100, stagger: 0.15, duration: 1, ease: "back.out(2.5)" },
+      {
+        opacity: 1,
+        x: 0,
+        stagger: 0.15,
+        duration: 1,
+        ease: "back.out(2.5)",
+      }
+    );
+  }, [categories]);
+
   const categoryLinks = () => {
-    console.log("categories", categories);
+    // console.log("categories", categories);
     return categories.map((category, index) => (
       <CategoryCard key={category.id} category={category} />
     ));
@@ -41,7 +59,9 @@ const CategoriesList = () => {
           Back
         </button>
       </div>
-      <div className="col-6">{categories && categoryLinks()}</div>
+      <div className="col-6" ref={cardRef}>
+        {categories && categoryLinks()}
+      </div>
     </div>
   );
 };
--- src/categories/CategoryCard.js
+++ src/categories/CategoryCard.js
@@ -1,19 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const CategoryCard = ({ category }) => {
-  console.log(category);
-
   return (
     <div className="card shadow my-2">
-      <div className="card-header">
+      <div className="card-header py-0">
         <div className="h3">{category.name}</div>
       </div>
       <div className="card-body">
         <p className="card-text">{category.description}</p>
 
         <Link
-          className="btn btn-primary"
+          className="col-2 btn btn-primary"
           to={`/categories/${category.id}/items`}
         >
           <div className="">Items</div>
