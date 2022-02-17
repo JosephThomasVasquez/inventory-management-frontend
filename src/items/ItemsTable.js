54416847214b423a2bb55b3b8c6de6f013d36122
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-02-16 22:44:53 -0800

    Created custom delete item confirmation with modal

--- src/items/ItemsTable.js
+++ src/items/ItemsTable.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import "./itemTable.style.css";
 import dayjs from "dayjs";
@@ -7,6 +7,8 @@ import gsap from "gsap";
 const ItemsTable = ({ items }) => {
   const itemRefs = useRef([]);
   itemRefs.current = [];
+
+  const [selectedItem, setSelectedItem] = useState({ name: "", id: "" });
 
   const tableHeaders = () => {
     if (items.length > 0) {
@@ -61,7 +63,70 @@ const ItemsTable = ({ items }) => {
 
   const handleDelete = ({ target }) => {
     console.log(target.id);
+
+    if (target.id) {
+      return deleteModal;
+    }
   };
+
+  const handleId = ({ target }) => {
+    console.log(target.id);
+
+    const foundItem = items.find((i) => i.id === Number(target.id));
+    console.log(foundItem);
+
+    setSelectedItem({ name: foundItem.name, id: foundItem.id });
+    console.log(selectedItem);
+  };
+
+  const deleteModal = (
+    <div
+      className="modal fade"
+      id="exampleModal"
+      tabIndex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Confirm Delete Item?
+            </h5>
+
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div>
+              This operation cannot be undone. Are you sure you want to delete
+              the item ( <span className="fw-bold">{selectedItem.name}</span> )?
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   const fillRows = () => {
     return items.map((item, index) => {
@@ -131,8 +196,12 @@ const ItemsTable = ({ items }) => {
           <td>
             <div
               className="col btn btn-outline-danger"
+              data-bs-toggle="modal"
+              data-bs-target="#exampleModal"
+              name={name}
               id={id}
-              onClick={handleDelete}
+              value={name}
+              onClick={handleId}
             >
               <i className="fa-solid fa-trash-can"></i>
             </div>
@@ -145,12 +214,15 @@ const ItemsTable = ({ items }) => {
   fillRows();
 
   return (
-    <table className="table">
-      <thead>
-        <tr>{tableHeaders()}</tr>
-      </thead>
-      <tbody>{fillRows()}</tbody>
-    </table>
+    <div>
+      {deleteModal}
+      <table className="table">
+        <thead>
+          <tr>{tableHeaders()}</tr>
+        </thead>
+        <tbody>{fillRows()}</tbody>
+      </table>
+    </div>
   );
 };
 
