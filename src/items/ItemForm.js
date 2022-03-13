3a136beef54e688298d33389e6b0d747032fa059
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2022-03-12 21:32:53 -0800

    Made item form responsive

--- src/items/ItemForm.js
+++ src/items/ItemForm.js
@@ -149,7 +149,7 @@ const ItemForm = ({ categories, errorHandler }) => {
   return (
     <div className="container">
       <div className="row my-auto">
-        <div className="col-11">
+        <div className="col-12 col-xl-10 col-lg-10 col-md-10 col-sm-10">
           <h2>
             {itemId ? "Edit Item" : "Create Item"}
             <span className="">
@@ -159,7 +159,7 @@ const ItemForm = ({ categories, errorHandler }) => {
           </h2>
         </div>
 
-        <div className="col-1 button-back">
+        <div className="col-1 button-back mb-3 mb-sm-3">
           <button
             type="button"
             className="btn btn-outline-primary"
@@ -173,7 +173,7 @@ const ItemForm = ({ categories, errorHandler }) => {
       <form className="col-12" onSubmit={handleSubmit}>
         {/* Main Image Url */}
         <div className="row">
-          <div className="col-6 mb-5">
+          <div className="col-12 col-md-6 mb-sm-5">
             {formData.main_imageUrl ? (
               <img
                 src={formData.main_imageUrl}
@@ -182,8 +182,8 @@ const ItemForm = ({ categories, errorHandler }) => {
               />
             ) : null}
           </div>
-          <div className="col-6 my-3" ref={addToRefs}>
-            <label htmlFor="main_imageUrl" className="form-label">
+          <div className="col-12 col-md-6 my-sm-3" ref={addToRefs}>
+            <label htmlFor="main_imageUrl" className="form-label fw-bold">
               Main Image
             </label>
             <input
@@ -203,14 +203,14 @@ const ItemForm = ({ categories, errorHandler }) => {
           <div className="col">
             <div className="list-group">
               <div className="row mb-3 item-edit-thumbnail" ref={addToRefs}>
-                <div className="col-1">
+                <div className="col-12 col-md-2">
                   <img
                     src={formData.image_1}
                     className="item-detail-thumbnail"
                   />
                 </div>
                 <div className="col me-3">
-                  <label htmlFor="image_1" className="form-label">
+                  <label htmlFor="image_1" className="form-label fw-bold">
                     Image 1
                   </label>
                   <input
@@ -227,14 +227,14 @@ const ItemForm = ({ categories, errorHandler }) => {
               </div>
 
               <div className="row mb-3 item-edit-thumbnail" ref={addToRefs}>
-                <div className="col-1">
+                <div className="col-12 col-md-2">
                   <img
                     src={formData.image_2}
                     className="item-detail-thumbnail"
                   />
                 </div>
                 <div className="col me-3">
-                  <label htmlFor="image_2" className="form-label">
+                  <label htmlFor="image_2" className="form-label fw-bold">
                     Image 2
                   </label>
                   <input
@@ -251,14 +251,14 @@ const ItemForm = ({ categories, errorHandler }) => {
               </div>
 
               <div className="row mb-3 item-edit-thumbnail" ref={addToRefs}>
-                <div className="col-1">
+                <div className="col-12 col-md-2">
                   <img
                     src={formData.image_3}
                     className="item-detail-thumbnail"
                   />
                 </div>
                 <div className="col me-3">
-                  <label htmlFor="image_3" className="form-label">
+                  <label htmlFor="image_3" className="form-label fw-bold">
                     Image 3
                   </label>
                   <input
@@ -275,14 +275,14 @@ const ItemForm = ({ categories, errorHandler }) => {
               </div>
 
               <div className="row mb-3 item-edit-thumbnail" ref={addToRefs}>
-                <div className="col-1">
+                <div className="col-12 col-md-2">
                   <img
                     src={formData.image_4}
                     className="item-detail-thumbnail"
                   />
                 </div>
                 <div className="col me-3">
-                  <label htmlFor="image_4" className="form-label">
+                  <label htmlFor="image_4" className="form-label fw-bold">
                     Image 4
                   </label>
                   <input
@@ -299,14 +299,14 @@ const ItemForm = ({ categories, errorHandler }) => {
               </div>
 
               <div className="row mb-3 item-edit-thumbnail" ref={addToRefs}>
-                <div className="col-1">
+                <div className="col-12 col-md-2">
                   <img
                     src={formData.image_5}
                     className="item-detail-thumbnail"
                   />
                 </div>
                 <div className="col me-3">
-                  <label htmlFor="image_5" className="form-label">
+                  <label htmlFor="image_5" className="form-label fw-bold">
                     Image 5
                   </label>
                   <input
@@ -325,10 +325,10 @@ const ItemForm = ({ categories, errorHandler }) => {
           </div>
         </div>
 
-        {/* Categories Dropdown */}
-        <div className="row">
-          <div className="col-6 my-3" ref={addToRefs}>
-            <label htmlFor="category_id" className="form-label">
+        <div className="row my-3">
+          {/* Categories Dropdown */}
+          <div className="col-12 col-md-6" ref={addToRefs}>
+            <label htmlFor="category_id" className="form-label fw-bold">
               Category
             </label>
             <select
@@ -341,154 +341,163 @@ const ItemForm = ({ categories, errorHandler }) => {
               {loadCategories}
             </select>
           </div>
+
+          {/* Name */}
+          <div className="col-12 col-md-6" ref={addToRefs}>
+            <label htmlFor="name" className="form-label fw-bold">
+              Item Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              id="name"
+              aria-describedby="name"
+              placeholder="Enter name of the item"
+              value={formData?.name}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
-        {/* Name */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="name" className="form-label">
-            Item Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            id="name"
-            aria-describedby="name"
-            placeholder="Enter name of the item"
-            value={formData?.name}
-            onChange={handleChange}
-          />
+        <div className="row">
+          {/* Description */}
+          <div className="mb-3" ref={addToRefs}>
+            <label htmlFor="description" className="form-label fw-bold">
+              Item Description
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              className="form-control"
+              id="description"
+              rows="8"
+              value={formData?.description}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
-        {/* Model */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="model" className="form-label">
-            Item Model
-          </label>
-          <input
-            type="text"
-            name="model"
-            className="form-control"
-            id="model"
-            aria-describedby="model"
-            placeholder="Enter model of the item"
-            value={formData?.model}
-            onChange={handleChange}
-          />
+        <div className="row">
+          {/* Price */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="price" className="form-label fw-bold">
+              Item price
+            </label>
+            <input
+              type="number"
+              name="price"
+              min="0"
+              step="any"
+              className="form-control"
+              id="price"
+              aria-describedby="price"
+              placeholder="Enter name of the item"
+              value={formData?.price}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* In Stock */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="quantity_in_stock" className="form-label fw-bold">
+              Quantity in stock
+            </label>
+            <input
+              type="number"
+              name="quantity_in_stock"
+              min="0"
+              step="any"
+              className="form-control"
+              id="quantity_in_stock"
+              aria-describedby="quantity_in_stock"
+              placeholder="Enter quanttiy in stock"
+              value={formData?.quantity_in_stock}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
-        {/* Description */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="description" className="form-label">
-            Item Description
-          </label>
-          <textarea
-            type="text"
-            name="description"
-            className="form-control"
-            id="description"
-            value={formData?.description}
-            onChange={handleChange}
-          />
+        <div className="row">
+          {/* Model */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="model" className="form-label fw-bold">
+              Item Model
+            </label>
+            <input
+              type="text"
+              name="model"
+              className="form-control"
+              id="model"
+              aria-describedby="model"
+              placeholder="Enter model of the item"
+              value={formData?.model}
+              onChange={handleChange}
+            />
+          </div>
+
+          {/* Sku / Product Number */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="sku" className="form-label fw-bold">
+              Sku / Product Number
+            </label>
+            <input
+              type="text"
+              name="sku"
+              className="form-control"
+              id="sku"
+              aria-describedby="sku"
+              placeholder="Enter a Sku or Product Number"
+              value={formData?.sku}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
         {/* Text Editor
         <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="description" className="form-label">
+          <label htmlFor="description" className="form-label fw-bold">
             Content
           </label>
           <TextEditor />
         </div> */}
 
-        {/* Release Date */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="release_date" className="form-label">
-            Release Date
-          </label>
-          <input
-            type="date"
-            name="release_date"
-            className="form-control"
-            id="release_date"
-            aria-describedby="release_date"
-            placeholder="Enter release date of the item"
-            value={formData?.release_date}
-            onChange={handleChange}
-          />
-        </div>
+        <div className="row">
+          {/* Release Date */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="release_date" className="form-label fw-bold">
+              Release Date
+            </label>
+            <input
+              type="date"
+              name="release_date"
+              className="form-control"
+              id="release_date"
+              aria-describedby="release_date"
+              placeholder="Enter release date of the item"
+              value={formData?.release_date}
+              onChange={handleChange}
+            />
+          </div>
 
-        {/* Sku / Product Number */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="sku" className="form-label">
-            Sku / Product Number
-          </label>
-          <input
-            type="text"
-            name="sku"
-            className="form-control"
-            id="sku"
-            aria-describedby="sku"
-            placeholder="Enter a Sku or Product Number"
-            value={formData?.sku}
-            onChange={handleChange}
-          />
-        </div>
-
-        {/* Price */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="price" className="form-label">
-            Item price
-          </label>
-          <input
-            type="number"
-            name="price"
-            min="0"
-            step="any"
-            className="form-control"
-            id="price"
-            aria-describedby="price"
-            placeholder="Enter name of the item"
-            value={formData?.price}
-            onChange={handleChange}
-          />
-        </div>
-
-        {/* In Stock */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="quantity_in_stock" className="form-label">
-            Quantity in stock
-          </label>
-          <input
-            type="number"
-            name="quantity_in_stock"
-            min="0"
-            step="any"
-            className="form-control"
-            id="quantity_in_stock"
-            aria-describedby="quantity_in_stock"
-            placeholder="Enter quanttiy in stock"
-            value={formData?.quantity_in_stock}
-            onChange={handleChange}
-          />
-        </div>
-
-        {/* In Stock */}
-        <div className="mb-3" ref={addToRefs}>
-          <label htmlFor="weight_in_lbs" className="form-label">
-            Weight in lbs.
-          </label>
-          <input
-            type="number"
-            name="weight_in_lbs"
-            min="0"
-            step="any"
-            className="form-control"
-            id="weight_in_lbs"
-            aria-describedby="weight_in_lbs"
-            placeholder="Enter weight in lbs."
-            value={formData?.weight_in_lbs}
-            onChange={handleChange}
-          />
+          {/* In Stock */}
+          <div className="col-12 col-md-6 mb-3" ref={addToRefs}>
+            <label htmlFor="weight_in_lbs" className="form-label fw-bold">
+              Weight in lbs.
+            </label>
+            <input
+              type="number"
+              name="weight_in_lbs"
+              min="0"
+              step="any"
+              className="form-control"
+              id="weight_in_lbs"
+              aria-describedby="weight_in_lbs"
+              placeholder="Enter weight in lbs."
+              value={formData?.weight_in_lbs}
+              onChange={handleChange}
+            />
+          </div>
         </div>
 
         {/* Checkbox */}
@@ -497,9 +506,10 @@ const ItemForm = ({ categories, errorHandler }) => {
             type="checkbox"
             className="form-check-input"
             id="exampleCheck1"
+            checked={true}
           />
           <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
+            Available
           </label>
         </div>
 
